e78d475cc545089137855663abcb22e2cb2fd6a5
Haykuhie <haykuhiek@gmail.com>
2025-11-04 15:01:09 +0400

    change vite config prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 import image from '@rollup/plugin-image';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command: _command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
     return {
         publicDir: 'public',
         build: {
@@ -62,16 +61,6 @@ export default defineConfig(({ command: _command, mode }) => {
             },
             port: 3000,
             strictPort: true,
-            proxy: {
-                '/api': {
-                    target:
-                        env.CODEMAX_API_TARGET ||
-                        'https://demo-codemax.azurewebsites.net/api',
-                    changeOrigin: true,
-                    ws: true,
-                    rewrite: path => path.replace(/^\/api/, ''),
-                },
-            },
         },
         base: '/Visualisation_Tool/',
         envPrefix: 'Visualization_Tool',
